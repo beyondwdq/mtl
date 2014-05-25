4d57b88488fa2bd0e1a2d16e3170497201b0b087
Danqi Wang <beyondwdq@gmail.com>
2014-05-25 13:39:46 +0800

    aded mtl/threading

---
+++ mtl/threading/taskqueue.h
@@ -0,0 +1,88 @@
+#ifndef MTL_THREADING_TASKQUEUE_H
+#define MTL_THREADING_TASKQUEUE_H
+
+#include <future>
+#include <deque>
+#include <functional>
+
+namespace mtl {
+namespace threading {
+
+// A concurrent task queue.
+// `T` is the type of the task's return type.
+template <typename T>
+class TaskQueue
+{
+public:
+  typedef std::packaged_task<T()> TTask;
+
+  TaskQueue() : stop_(false), no_more_task_(false) {}
+
+  // Put a task into queue and return a future
+  std::future<T> put(std::function<T()> task)
+  {
+    std::unique_lock<std::mutex> lock(access_);
+    tasks_.push_back(TTask(task));
+    auto ret = tasks_.back().get_future();
+    cond_.notify_one();
+    return ret;
+  }
+
+  // Add a task into queue, no future returned.
+  void add(std::function<T()> task)
+  {
+    std::unique_lock<std::mutex> lock(access_);
+    tasks_.push_back(TTask(task));
+    cond_.notify_one();
+  }
+
+  // Get a task from queue
+  TTask get()
+  {
+    std::unique_lock<std::mutex> lock(access_);
+    while (!stop_ && !no_more_task_ && tasks_.empty()) {
+      cond_.wait(lock);
+    }
+    TTask task;
+    if (!stop_ && !tasks_.empty()) {
+      task = std::move(tasks_.front());
+      tasks_.pop_front();
+    }
+    return task;
+  }
+
+  // Stop the queue regardless whether the queue is empty or not
+  void stop()
+  {
+    {
+      std::unique_lock<std::mutex> lock(access_);
+      stop_ = true;
+    }
+    cond_.notify_all();
+  }
+
+  // Indicates that no more task will be added to the queue.
+  // Invocation of get() will not keep the thread waiting if 
+  // the queue is empty.
+  void no_more_task() 
+  {
+    {
+      std::unique_lock<std::mutex> lock(access_);
+      no_more_task_ = true;
+    }
+    cond_.notify_all();
+  }
+
+
+private:
+	bool stop_;
+  bool no_more_task_;
+	mutable std::mutex      access_;
+	std::deque<TTask>       tasks_;
+	std::condition_variable cond_;
+};
+
+} // namespace threading
+} // namespace mtl
+
+#endif
---
+++ mtl/threading/threadpool.h
@@ -0,0 +1,90 @@
+#ifndef MTL_THREADING_THREADPOOL_H
+#define MTL_THREADING_THREADPOOL_H
+
+#include <future>
+#include <thread>
+#include <functional>
+#include <vector>
+
+#include "taskqueue.h"
+
+namespace mtl {
+namespace threading {
+
+template <typename T>
+class ThreadPool {
+
+public:
+  typedef TaskQueue<T> TQueue;
+
+  ~ThreadPool() 
+  {
+    stop();
+  }
+
+  // Start threads
+  void start(unsigned num) 
+  {
+    assert (num > 0);
+    while(num-- >0) {
+      addWorker();
+    }
+  }
+
+  // Run a task and return a future.
+  // `task` must be convertable to std::function<T()>.
+  std::future<T> runTask(std::function<T()> task) 
+  {
+    return queue_.put(task);
+  }
+
+  // Run a task, no future returned.
+  void addTask(std::function<T()> task) 
+  {
+    queue_.add(task);
+  }
+
+  // Notify threads to exit and wait for them.
+  void stop() 
+  {
+    if (pool_.empty()) return;
+
+    queue_.stop();
+    for(std::thread &t : pool_) {
+      t.join();
+    }
+    pool_.clear();
+  }
+
+  // Wait for all tasks to finish and then stop the threads.
+  void wait()
+  {
+    queue_.no_more_task();
+    for(std::thread &t : pool_) {
+      t.join();
+    }
+    pool_.clear();
+  }
+
+private:
+  void addWorker() {
+    std::thread t([this]() {
+      while (true) {
+        typename TQueue::TTask task(queue_.get());
+        if (!task.valid()) break;
+        task();
+      }
+      });
+    pool_.push_back(std::move(t));
+  }
+
+private:
+  TQueue queue_;
+	std::vector<std::thread> pool_;
+};
+
+} // namespace threading
+} // namespace mtl
+
+#endif
+
